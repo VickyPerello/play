405d244dc91ad8bb7728b574ccf94e39ea317e5b
Vicky <victoriajperello@gmail.com>
2018-09-21 20:27:32 -0300

    refactor

--- src/Play.java
+++ src/Play.java
@@ -2,20 +2,9 @@ public class Play {
 
     public static void main(String [ ] args)
     {
-        problem1();
+        Problem1.problem1();
 
     }
 
-    static public void problem1() {
-        //Multiples of 3 and 5
-        int sum = 0;
 
-        for (int i=0; i<1000; i++){
-            if (i%3==0 || i%5==0){
-                sum+=i;
-            }
-        }
-
-        System.out.println("sum"+sum);
-    }
 }
