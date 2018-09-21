8310aa676195b6fd923c338f49c3a5a4692c368a
Vicky <victoriajperello@gmail.com>
2018-09-21 20:23:47 -0300

    first problem

---
+++ src/Play.java
@@ -0,0 +1,21 @@
+public class Play {
+
+    public static void main(String [ ] args)
+    {
+        problem1();
+
+    }
+
+    static public void problem1() {
+        //Multiples of 3 and 5
+        int sum = 0;
+
+        for (int i=0; i<1000; i++){
+            if (i%3==0 || i%5==0){
+                sum+=i;
+            }
+        }
+
+        System.out.println("sum"+sum);
+    }
+}
